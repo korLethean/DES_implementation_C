7a85ce0d3cba040cb8c4f393df1eb4566af2685d
Lethean <hdgwon@naver.com>
2019-01-08 15:25:05 +0900

    array index exception check algorithm fixed

--- DES.c
+++ DES.c
@@ -20,10 +20,10 @@ des_err split(const int in_block_length, const int out_block_length, int *in_blo
 
 des_err permute(const int in_block_length, const int out_block_length, const int table_size, int *in_block, int *out_block, const int *permute_table)
 {
-	if(in_block_length != 64 || in_block_length != 56 || out_block_length != 56 || out_block_length != 48)
+	if(!(in_block_length == 64 || in_block_length == 56 || out_block_length == 56 || out_block_length == 48))
 		return DES_BLK_LEN_ERR;
 
-	if(table_size != 32 || table_size != 48 || table_size != 68)
+	if(!(table_size == 32 || table_size == 48 || table_size == 64))
 		return DES_TABLE_LEN_ERR;
 
 	for(int i = 0 ; i < table_size ; i++)
@@ -34,8 +34,13 @@ des_err permute(const int in_block_length, const int out_block_length, const int
 
 des_err key_generator(const int pk_size, const int rk_size, int *key_with_parities, int (*round_keys)[48])
 {
+	des_err error_code;
+
 	if(pk_size != 64 || rk_size != 48)
-		return DES_BLK_LEN_ERR;
+	{
+		error_code = DES_BLK_LEN_ERR;
+		return error_code;
+	}
 
 	const int prk_size = 56;
 
@@ -44,19 +49,25 @@ des_err key_generator(const int pk_size, const int rk_size, int *key_with_pariti
 	int right_key[prk_size / 2];
 	int pre_round_key[prk_size];
 
-	permute(pk_size, prk_size, ParityDropTableSize, key_with_parities, cipher_key, ParityDropTable);
+	error_code = permute(pk_size, prk_size, ParityDropTableSize, key_with_parities, cipher_key, ParityDropTable);
+	if(error_code != DES_SUCCESS)
+		return error_code;
 
-	split(prk_size, prk_size / 2, cipher_key, left_key, right_key);
+	error_code = split(prk_size, prk_size / 2, cipher_key, left_key, right_key);
+	if(error_code != DES_SUCCESS)
+		return error_code;
 
 	for(int i = 0 ; i < 16 ; i++)
 	{
 		// shiftleft
 		// shiftleft
 		// combine
-		permute(prk_size, rk_size, KeyCompressionTableSize, pre_round_key, round_keys[i], KeyCompressionTable);
+		error_code = permute(prk_size, rk_size, KeyCompressionTableSize, pre_round_key, round_keys[i], KeyCompressionTable);
+		if(error_code != DES_SUCCESS)
+			return error_code;
 	}
 
-	return DES_SUCCESS;
+	return error_code;
 }
 
 int H2B(char value)
